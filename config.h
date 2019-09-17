2545e5f43083369cadc067c65663868befa3e1f7
ConsciousCode <conscious.code@gmail.com>
2019-09-17 14:14:16 -0600

    Made some macros to make control flow more obvious, restructured if-else chains to essentially a dispatch table

--- config.h
+++ config.h
@@ -7,6 +7,9 @@
 // i2c address, mostly arbitrary
 #define I2C_ADDR 0x71
 
+// 0x15 is CEC's "no address" or broadcast address
+#define CEC_ADDR_DEFAULT 0x15
+
 // Pin assignments
 #define SCL PORTB2
 #define SDA PORTB1
@@ -30,9 +33,15 @@
 #define LOAD_TAKEN 1 // Load taken into the buffer
 #define CEC_SEND 2 // Write the buffer to the CEC bus
 #define SET_ADDR 3 // Set CEC address (for reading from CEC)
+#define TOGGLE_MON 4 // Toggle monitor mode
+#define TOGGLE_DEBUG 5 // Toggle debug mode
 
 // Note: Changing these only changes the calculation of
 //  "jiffies" for timing, it won't affect the actual
 //  timer control flags.
 #define CLOCK_HZ 8000000
 #define CLOCK_DIV 1024
+
+// Whether or not to use pin 3 to indicate a "dirty" buffer
+// Note that this is optional, comment to remove the feature
+#define DIRTY_PIN PORTB3
